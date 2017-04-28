5e4604e61a6ca63ea2c06af72da973e2962ae4ce
zqh <zhangqinghao@123juzi.com>
2017-04-28 11:15:59 +0800

    study

--- es6/symbol.js
+++ es6/symbol.js
@@ -7,3 +7,51 @@ var a={};
 a.mySymbol='hello';
 a[mySymbol]
 a['mySymbol']
+
+
+log.levels={
+	DEBUG:Symbol('debug'),
+	INFO:Symbol('info'),
+	WARN:Symbol('warn')
+};
+
+
+const color_red=Symbol('red');
+const color_green=Symbol('green');
+function getComplement(color){
+	switch (color){
+		case color_red:
+		return color_green;
+		case color_green:
+		return color_red;
+		default:
+		throw new Error('undefined color');
+	}
+}
+
+getComplement(color_red)
+
+
+function getArea(shape,options){
+	var area=0;
+	switch(shape){
+		case "triangle":
+		area=.5*options.width*options.height;
+		break;
+	}
+	return area;
+}
+
+getArea('triangle',{width:100,height:100});
+
+var obj={};
+var a=Symbol();
+var b=Symbol();
+
+
+obj[a]='hello';
+obj[b]="world";
+console.log(obj[a]);
+for(var i in obj){
+	console.log(i);
+}
