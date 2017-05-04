1af85eb63248943eeb432256db081c0e140ce6f2
zqh <zhangqinghao@123juzi.com>
2017-05-04 21:23:31 +0800

    proxy

--- es6/set_and_map.js
+++ es6/set_and_map.js
@@ -253,3 +253,79 @@ function sum(left,right){
 var proxy=new Proxy(sum,twice);
 proxy(1,2);
 
+var handler={
+	has(target,key){
+		if(key[0]==="_"){
+			return false;
+		}
+		return key in target;
+	}
+};
+var target={
+	_prop:'foo',
+	prop:"foo"
+};
+var proxy=new Proxy(target,handler);
+
+
+var obj ={a:10};
+Object.preventExtensions(obj);
+var p= new Proxy(obj,{
+	has:function(target,prop){
+		return false;
+	}
+})
+
+
+let stu1={name:'zhangsan',score:59};
+let stu2={name:'lisi',score:99};
+let handler={
+	has(target,prop){
+		if(prop==='score'&&target[prop]<60){
+			console.log(`${target.name}不及格`);
+			return false;
+		}
+		return prop in target;
+	}
+}
+let oproxy1= new Proxy(stu1,handler);
+let oproxy2=new Proxy(stu2,handler);
+'score' in oproxy1;
+for(let a in oproxy1){
+	console.log(oproxy1[a]);
+}
+
+var handler={
+	construct(target,args,newTarget){
+		return new target(...args);
+	}
+}
+
+
+var p=new Proxy(function(){},{
+	construct:function(target,args){
+		console.log('called: '+args.join(", "));
+		return {value:args[0]*10};
+	}
+});
+(new p(1)).value;
+
+
+
+var handler={
+	deleteProperty(target,key){
+		invariant(key,'delete');
+		return true;
+	}
+};
+
+function invariant(key,action){
+	if(key[0]==="_"){
+		throw new Error(`Invalid attempt to ${action} private "${key}" property`);
+	}
+}
+var target={_prop:'foo'};
+var proxy=new Proxy(target,handler);
+delete proxy._prop;
+
+
