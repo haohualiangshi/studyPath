6b12b0dd6d284d15e32401fe130cbe664ad8b5ff
zqh <zhangqinghao@123juzi.com>
2017-05-04 20:19:58 +0800

    proxy

--- es6/set_and_map.js
+++ es6/set_and_map.js
@@ -75,36 +75,181 @@ function Student(name, age, grade, school) {
 }
 
 
-var p1=new Person('jake',10);
+var p1 = new Person('jake', 10);
 p1.sayHello();
-var s1=new Student('tom',13,6,'清华小学');
+var s1 = new Student('tom', 13, 6, '清华小学');
 
 
 
 
 
 var handler = {
-  get: function(target, name) {
-  	console.log('get');
-    if (name === 'prototype') {
-      return Object.prototype;
+    get: function(target, name) {
+        console.log('get');
+        if (name === 'prototype') {
+            return Object.prototype;
+        }
+        return 'Hello, ' + name;
+    },
+
+    // apply: function(target, thisBinding, args) {
+    // 	console.log('apply');
+    //   return args[0];
+    // },
+
+    construct: function(target, args) {
+        console.log('construct;');
+        return { value: args[1] };
     }
-    return 'Hello, ' + name;
-  },
-
-  // apply: function(target, thisBinding, args) {
-  // 	console.log('apply');
-  //   return args[0];
-  // },
-
-  construct: function(target, args) {
-  	console.log('construct;');
-    return {value: args[1]};
-  }
 };
 
 var fproxy = new Proxy(function(x, y) {
-  return x + y;
+    return x + y;
 }, handler);
 
 fproxy(1, 2) // 1
+
+var person = {
+    name: "zhangshan",
+}
+var proxy = new Proxy(person, {
+    get: function(target, property) {
+        if (property in target) {
+            return target[property];
+        } else {
+            throw new ReferenceError("Property \"" + property + "\"does not exist .");
+        }
+    }
+})
+
+
+function createArray(...elements) {
+    let handler = {
+        get(target, propKey, receiver) {
+            let index = Number(propKey);
+            if (index < 0) {
+                propKey = String(target.length + index);
+            }
+            return Reflect.get(target, propKey, receiver);
+        }
+    }
+    let target = [];
+    target.push(...elements);
+    return new Proxy(target, handler);
+}
+let arr = createArray('a', "b", "c");
+arr[-1];
+
+
+var pipe = (function() {
+    return function(value) {
+        var funcStack = [];
+        var oproxy = new Proxy({}, {
+            get: function(pipeiObject, fnName) {
+            	console.log(pipeiObject, fnName);
+                if (fnName === 'get') {
+                    return funcStack.reduce(function(val, fn) {
+                        return fn(val);
+                    }, value);
+                }
+                funcStack.push(window[fnName]);
+                return oproxy;
+            }
+        });
+
+        return oproxy;
+    }
+}());
+var double = n => n * 2;
+var pow=n=>n*n;
+var reverseInt=n=>n.toString().split("").reverse().join("")|0;
+pipe(3).double.pow.reverseInt.get;
+
+
+
+const dom = new Proxy({},{
+	get(target,property){
+		return function(attrs={},...children){
+			const el=document.createElement(property);
+			for(let prop of Object.keys(attrs)){
+				el.setAttribute(prop,attrs[prop]);
+			}
+			for(let child of children){
+				if(typeof child==="string"){
+					child=document.createTextNode(child);
+				}
+				el.appendChild(child);
+			}
+			return el;
+		}
+	}
+});
+const e1=dom.div({},'indser')
+const el=dom.div({},'hello,my name is ',dom.a({href:'www.baidu.com'},'mark'),
+	'. i like:',
+	dom.ul({},dom.li({},'The web'),dom.li({},'food'),dom.li({},'...that beautiful'))
+	)
+
+let validator={
+	set:function(obj,prop,value){
+		if(prop ==='age'){
+			if(!Number.isInteger(value)){
+				throw new TypeError('The age is not an integer');
+			}
+			if(value > 200){
+				throw new RangeError('this age seems invalid');
+			}
+		}
+		obj[prop]=value;
+	}
+}
+let person = new Proxy({},validator);
+person.age=100
+
+
+
+var handler = {
+    get(target, key) {
+        invariant(key, 'get');
+        return target[key];
+    },
+    set(target, key, value) {
+        invariant(key, 'set');
+        target[key] = value;
+        return true;
+    }
+};
+
+function invariant(key, action) {
+    if (key[0] === '_') {
+        throw new Error(`invalid attempt to ${action} provate "${key}" property`);
+    }
+}
+var target = {};
+var proxy = new Proxy(target, handler);
+
+
+
+var target=function(){
+	return 'I am the target';
+};
+var handler={
+	apply:function(){
+		return 'I am the proxy';
+	}
+}
+
+var p=new Proxy(target,handler);
+
+
+var twice={
+	apply(target,ctx,args){
+		return Reflect.apply(...arguments)*2;
+	}
+};
+function sum(left,right){
+	return left+right;	
+}
+var proxy=new Proxy(sum,twice);
+proxy(1,2);
+
