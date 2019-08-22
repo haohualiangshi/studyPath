bcff343974a90d1791794125c74d80c59c369391
zhangqinghao <zhangqinghao@123juzi.com>
2019-08-22 11:27:50 +0800

    本地模拟接口

--- python_webserve/business/one_serve.py
+++ python_webserve/business/one_serve.py
@@ -1,10 +1,29 @@
 from flask import Flask
 from global_env import getRootPath
 from flask import make_response
+import random
 
 import os
 
 app = Flask(__name__)
+
+
+def operateFile(filePath):
+    file_object = open(filePath)
+    try:
+        result_text = file_object.read()
+    finally:
+        file_object.close()
+        #
+    rst = make_response(result_text)
+    rst.headers['Access-Control-Allow-Origin'] = '*'
+    rst.headers['Access-Control-Allow-Methods'] = 'PUT,GET,POST,DELETE,OPTIONS'
+    rst.headers['Content-type'] = 'application/json;chartset=utf-8'
+    # allow_headers = "Referer,Accept,Origin,User-Agent"
+    # rst.headers['Access-Control-Allow-Headers'] = allow_headers
+    print(rst)
+    return rst, 200
+
 
 @app.route("/",methods=['GET','POST'])
 def defaultReturn():
@@ -17,7 +36,7 @@ def defaultReturn():
     return rst, 200
 
 @app.route('/result', methods=['GET', 'POST'])
-def index():
+def result():
     rootPath = getRootPath()
     filePath = os.path.join(rootPath,"data_json","test_1.json")
     print(filePath)
@@ -36,6 +55,55 @@ def index():
     print(rst)
     return rst, 200
     #return result_text, 201 #, {'Content-Type': 'application/json'}
+
+
+# 网红注册邮箱验证
+@app.route('/user/getCodeByEmail',methods=['GET'])
+def verifyEmailResult():
+    rootPath = getRootPath()
+    verifyEmailJsonArr = ['verifyEmailFail.json','verifyEmailSuccess.json']
+    jsonFilrIndex = random.randint(0,len(verifyEmailJsonArr)-1)
+    #
+    print(jsonFilrIndex)
+    filePath = os.path.join(rootPath, "data_json", verifyEmailJsonArr[jsonFilrIndex])
+    print(filePath)
+    return operateFile(filePath)
+# 网红注册
+@app.route('/user/register',methods=['POST',"GET"])
+def influuRegister():
+    rootPath = getRootPath()
+    verifyEmailJsonArr = ['emailAlreadyHas.json','registerFail.json',"registeSuccess.json","userNameAlreadyHas.json"]
+    jsonFilrIndex = random.randint(0,len(verifyEmailJsonArr)-1)
+    #
+    print(jsonFilrIndex)
+    filePath = os.path.join(rootPath, "data_json","registerJsons", verifyEmailJsonArr[jsonFilrIndex])
+    print(filePath)
+    return operateFile(filePath)
+
+# 网红登录认证
+@app.route('/authentication/form',methods=['POST',"GET"])
+def user_author():
+    rootPath = getRootPath()
+    verifyEmailJsonArr = ['author_success.json','author_times_than_three.json',"email_or_pwd_fail.json"]
+    jsonFilrIndex = random.randint(0,len(verifyEmailJsonArr)-1)
+    #
+    print(jsonFilrIndex)
+    filePath = os.path.join(rootPath, "data_json","author", verifyEmailJsonArr[jsonFilrIndex])
+    print(filePath)
+    return operateFile(filePath)
+
+# 网红登录token
+@app.route('/oauth/token',methods=['POST',"GET"])
+def user_token():
+    rootPath = getRootPath()
+    verifyEmailJsonArr = ['author_token_success.json',"login_success.json"]
+    jsonFilrIndex = random.randint(0,len(verifyEmailJsonArr)-1)
+    #
+    print(jsonFilrIndex)
+    filePath = os.path.join(rootPath, "data_json","author_token", verifyEmailJsonArr[jsonFilrIndex])
+    print(filePath)
+    return operateFile(filePath)
+
 
 
 
