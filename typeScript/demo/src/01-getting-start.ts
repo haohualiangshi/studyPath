f1175248e9224ef900937c08a8c96b2688aaab2d
ahao <13525006097@163.com>
2021-08-25 14:41:26 +0800

    typescript基础学习

---
+++ typeScript/demo/src/01-getting-start.ts
@@ -0,0 +1,13 @@
+/*
+ * @Descripttion: 快速上手
+ * @version: 
+ * @Author: ahao
+ * @Date: 2021-08-25 10:33:07
+ * @LastEditors: ahao
+ * @LastEditTime: 2021-08-25 14:23:37
+ */
+
+const hello = (name: string) => {
+    console.log(`Hello, ${name}`);
+}
+hello('ahao');
---
+++ typeScript/demo/src/02-primitive-types.ts
@@ -0,0 +1,23 @@
+/*
+ * @Descripttion: typescript数据类型
+ * @version: 
+ * @Author: ahao
+ * @Date: 2021-08-25 14:29:59
+ * @LastEditors: ahao
+ * @LastEditTime: 2021-08-25 14:39:18
+ */
+
+// 原始类型
+const a: string = 'foobar';
+const b: number = 100; //NAN
+const c: boolean = true;
+
+// 非严格模式赋值否可以为null；严格模式不行
+// const d: boolean = null;
+const e: void = undefined;
+const f: null = null;
+const g: undefined = undefined;
+const h: symbol = Symbol();
+
+
+
