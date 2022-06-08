13a52e63f814ec5a1801f575aac0963c8ce80b3f
ahao <1139902132@qq.com>
2022-06-08 22:57:37 +0800

    feat: typescript学习

---
+++ typeScript/demo/src/01-getting-start.js
@@ -0,0 +1,13 @@
+/*
+ * @Descripttion: 快速上手
+ * @version:
+ * @Author: ahao
+ * @Date: 2021-08-25 10:33:07
+ * @LastEditors: ahao
+ * @LastEditTime: 2022-05-30 10:08:01
+ */
+var hello = function (name) {
+    console.log("Hello, ".concat(name));
+};
+hello('ahao');
+// const num:number='';
--- typeScript/demo/src/01-getting-start.ts
+++ typeScript/demo/src/01-getting-start.ts
@@ -1,13 +1,27 @@
 /*
  * @Descripttion: 快速上手
- * @version: 
+ * @version:
  * @Author: ahao
  * @Date: 2021-08-25 10:33:07
  * @LastEditors: ahao
- * @LastEditTime: 2021-08-25 14:23:37
+ * @LastEditTime: 2022-05-30 10:33:20
  */
 
 const hello = (name: string) => {
-    console.log(`Hello, ${name}`);
-}
-hello('ahao');
+  console.log(`Hello, ${name}`);
+};
+hello("ahao");
+// const num:number='';
+const firstName: string = "Captain";
+const familyName: string = String("zhang");
+const fullName = `my name is ${familyName} ${firstName}`;
+console.log(fullName);
+
+const integer: number = 6;
+const integer2: number = Number(42);
+const decimal: number = 3.14;
+const binary: number = 0b1010;
+const octal: number = 0o744;
+const hex: number = 0xf00d;
+const big: bigint = 100n;
+console.log(binary, octal, hex, big);
